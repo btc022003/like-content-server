6b47032b47054dd9f89d0981931861788f9d2740
Arivin <btc022003@gmail.com>
2019-07-20 08:58:31 +0800

    add default value for article info

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,4 +5,14 @@ class Article < ApplicationRecord
   has_many :article_collections
 
   validates :title, presence: true
+
+  after_initialize :set_default_attr
+
+  def set_default_attr
+    self.descriptions ||= '' if has_attribute?(:descriptions)
+    self.cover_img ||= '' if has_attribute?(:cover_img)
+    self.count_of_article_likes ||= 0 if has_attribute?(:count_of_article_likes)
+    self.count_of_article_collections ||= 0 if has_attribute?(:count_of_article_collections)
+    self.count_of_article_comments ||= 0 if has_attribute?(:count_of_article_comments)
+  end
 end
